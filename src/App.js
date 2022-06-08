96867aefb0072b64c4a7f0235316cf5341ef010b
Marat <malik234@mail.ru>
2022-06-08 15:01:46 +0300

    sseee

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import Counter from "./components/Counter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
@@ -8,36 +8,46 @@ import Myinput from "./components/UI/input/Myinput";
 import PostFor from "./components/PostFor";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/MyModal/MyModal";
+import {usePosts} from "./hooks/usePost";
+import axios from "axios";
+import PosService from "./components/API/PosService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import postItem from "./components/PostItem";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
-    const [posts,setPosts] = useState([
-        {id:1, title: 'js', body: 'desc'},
-        {id:2, title: 'css', body: 'desc'},
-        {id:3, title: 'html', body: 'desc'},
-        {id:4, title: 'php', body: 'desc'}
-    ])
-
+    const [posts,setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal,setModal] = useState(false);
+    const [totalPages,setTotalPages] = useState(0);
+    const [limit,setLimit] = useState(10);
+    const [page,setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort,filter.query)
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {})
 
+    let pagesArray = getPagesArray(totalPages);
 
+    async function fetchPostG(){
+        const response = await PosService.getAll(limit,page)
+        setPosts(response.data)
+        const totalCount = (response.headers['x-total-count'])
+        setTotalPages(getPageCount(totalCount,limit))
+    }
 
-    const sortedPosts = useMemo(()=>{
-        console.log("OTRABOTAL")
-        if (filter.sort){
-            return [...posts].sort((a,b)=>a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    },[filter.sort,posts])
-
-    {/*Функция поиска*/}
-    const sortedAndSearchedPosts = useMemo(()=>{
-        return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query.toLowerCase()));
-    },[filter.query,sortedPosts])
+   useEffect(  () => {
+       fetchPostG()
+   }, [page])
 
     {/*Функция принимает новый пост, где разварачиваем массив*/}
     const createPost = (newPost)=>{
         setPosts([...posts,newPost])
+        setModal(false);
     }
+
+
+
 
     {/*Функция обратного вызова для удаления поста из массива*/}
     const remove = (post) => {
@@ -47,15 +57,45 @@ function App() {
         setPosts(posts.filter(p=>p.id!==post.id));
     }
 
-
+    const changePage = (page) =>{
+        setPage(page)
+    }
 
       return (
        <div className="App">
-            <PostFor create={createPost} />
+
+           <MyButton onClick={()=>setModal(true)}>
+               Создать пользователя
+           </MyButton>
+           <MyModal setVisible={setModal} visible={modal}>
+               <PostFor create={createPost} />
+               <MyButton onClick={()=>setModal(false)}>
+                   Закрыть
+               </MyButton>
+           </MyModal>
+
+
            <hr style={{margin: '15px 0'}}/>
-          <PostFilter filter={filter} setFilter={setFilter} />
-           <PostList remove={remove} posts={sortedAndSearchedPosts} title="Posts1"  />
-        </div>
+           <PostFilter filter={filter} setFilter={setFilter} />
+           {postError &&
+               <h1>Error "{postError}"</h1>
+           }
+
+           {isPostLoading
+               ? <div style={{display: 'flex',justifyContent: 'center',marginTop:'10px'}}><Loader/></div>
+               : <PostList remove={remove} posts={sortedAndSearchedPosts} title="Posts1"  />
+           }
+               <div className="page__wrapper">
+                   {pagesArray.map(p=>
+                      <span
+                          onClick={()=>changePage(p)}
+                          key={p}
+                          className={page === p ? 'page page__current' : 'page'}>
+                          {p}
+                      </span>
+                   )}
+               </div>
+          </div>
       );
     }
 
